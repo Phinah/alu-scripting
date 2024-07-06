f0620bfdbcce636e98e1c71678f69082577f1a37
phinah <phinah.mahoro10@gmail.com>
2024-07-06 23:40:11 +0200

    fix: the pep8 validation

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""function that prints the titles of the first 10 hot"""
+"""
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """function that prints the titles of the first 10 hot"""
-    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
